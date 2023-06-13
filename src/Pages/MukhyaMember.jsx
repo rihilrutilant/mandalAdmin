bc0062ce0a22e7c35dc8cec53de733acab0be85b
Jenish <jenish.rutilant@gmail.com>
2023-06-13 18:05:45 +0530

    fdsfsd

--- src/Pages/MukhyaMember.jsx
+++ src/Pages/MukhyaMember.jsx
@@ -4,7 +4,8 @@ import SideBar from "../component/SideBar"
 import wl from "../assets/whatsapp.png"
 import Navbar from '../component/Navbar'
 import axios from 'axios'
-import apiconst from '../keys'
+import {apiconst} from '../keys'
+import { BiEditAlt } from 'react-icons/bi'
 
 const MukhyaMember = () => {
 
@@ -70,6 +71,52 @@ const MukhyaMember = () => {
   }
   // ---------------- Add member--------------------
 
+  // ---------------- Edit member--------------------
+
+  const [editMember, setEditMember] = useState({
+    mukhiya_mobile_no: "",
+    password: ""
+  })
+
+  const ref = useRef(null);
+
+  const updateMember = (currentRest) => {
+    ref.current.click();
+    setEditMember({
+      mukhiya_mobile_no: currentRest.mukhiya_mobile_no,
+      password: currentRest.password
+    })
+  }
+
+  const updateAllMember = (e) => {
+    e.preventDefault()
+
+    let config = {
+      method: 'put',
+      maxBodyLength: Infinity,
+      url: apiconst.edit_mukhya_member,
+      headers: {
+        'auth-token': localStorage.getItem('Admin_Token'),
+        'Content-Type': 'application/json'
+      },
+      data: editMember
+    };
+
+    axios.request(config)
+      .then((response) => {
+        refClose.current.click()
+      })
+      .catch((error) => {
+        alert("There is something wrong entry")
+      });
+  }
+
+  const editChange = (e) => {
+    setEditMember({ ...editMember, [e.target.name]: e.target.value })
+  }
+
+  // ---------------- Edit member--------------------
+
   return (
     <>
       <div className="flex-section">
@@ -89,6 +136,7 @@ const MukhyaMember = () => {
                 <th scope="col">Member ID</th>
                 <th scope="col">Password</th>
                 <th scope="col"></th>
+                <th scope="col"></th>
               </tr>
             </thead>
             <tbody>
@@ -99,6 +147,7 @@ const MukhyaMember = () => {
                     <td>{item.mukhiya_mobile_no}</td>
                     <td>{item.member_id}</td>
                     <td>{item.member_password}</td>
+                    <td><BiEditAlt className='cursor-pointer' onClick={() => updateMember(item)} /></td>
                     <td><img src={wl} className='wl-logo' alt="" /></td>
                   </tr>
                 ))
@@ -130,6 +179,31 @@ const MukhyaMember = () => {
           </div>
         </div>
         {/* --------- Add Data --------------- */}
+
+        {/* --------- Update Data --------------- */}
+        <button type="button" style={{ display: 'none' }} ref={ref} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal123">
+          Launch demo modal
+        </button>
+        <div className="modal fade" id="exampleModal123" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title" id="exampleModalLabel">Update Data</h4>
+                <button type="button" className="btn-close" ref={refClose} data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <p className='modal-title-name'>Mukhiya Mobile No</p>
+                <input type="text" className='input-tag' onChange={editChange} name='mukhiya_mobile_no' value={editMember.mukhiya_mobile_no} />
+                <p className='modal-title-name'>Password</p>
+                <input type="password" className='input-tag' onChange={editChange} name='password' value={editMember.password} />
+              </div>
+              <div className="modal-footer">
+                <button onClick={updateAllMember} type="submit" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* --------- Update Data --------------- */}
 
       </div>
     </>
