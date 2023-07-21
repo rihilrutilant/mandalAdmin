5b3830cedf281fa2b60f93d6cfea7478aff7e451
krishnaV-tm <krishnaV.technomargin@gmail.com>
2023-07-21 14:30:47 +0530

    motivation create - delete api

--- src/Pages/Motivation.jsx
+++ src/Pages/Motivation.jsx
@@ -1,74 +1,81 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import '../style/MukhyaMember.css'
 import SideBar from "../component/SideBar"
 import Navbar from '../component/Navbar'
 import { useState } from 'react';
+import makeAPIRequest from '../Globle/apiCall';
+import { apiconst } from '../Globle/keys';
+import { AiFillDelete } from 'react-icons/ai';
 
 
 const Motivation = () => {
 
-    const [lastId, setlastId] = useState()
+
+    //---------------------- get all motivation ----------------------------
+    const [motivation, setMotivation] = useState([])
+    const getAllMotivations = useCallback(() => {
+        makeAPIRequest("get", apiconst.getAllMotivation, null, null, null)
+            .then((response) => {
+                setMotivation(response.data.motivationData)
+            }).catch(() => {
+                alert("There is something wrong entry")
+            });
+    }, [])
+
+    //---------------------- get all motivation ----------------------------
 
 
-    const [allData, setallData] = useState({
-        mukhiya_mobile_no: "",
-        password: ""
+    // -------------------------- Add motivation ------------------------
+
+    const refClose = useRef(null)
+    const [addMotivations, setAddMotivations] = useState({
+        motivation_By: "",
+        notes: ""
     })
+    const [motivatorImg, setMotivatorImg] = useState()
 
-    const refClose = useRef(null);
-    const refClose1 = useRef(null);
-
-    const addData = (id) => {
-        // const { mukhiya_mobile_no, password } = allData
-        // const member_id = id
-        // let data = {
-        //     mukhiya_mobile_no,
-        //     member_id,
-        //     password
-        // }
-        // makeAPIRequest('post', apiconst.create_mukhya_member, data, null, null)
-        //     .then(function (response) {
-        //         refClose1.current.click()
-        //         fetchallmembers()
-        //     })
-        //     .catch(function (error) {
-        //         alert("Please Enter Valid Data")
-        //     });
+    const onChanges = (e) => {
+        setAddMotivations({ ...addMotivations, [e.target.name]: e.target.value })
     }
 
-    const onChangesholiday = (e) => {
-        setallData(() => ({ ...allData, [e.target.name]: e.target.value }))
+    const addMotivationsByMotivators = () => {
+        const formData = new FormData();
+        formData.append("photo", motivatorImg)
+        formData.append("notes", addMotivations.notes)
+        formData.append("motivation_By", addMotivations.motivation_By)
+
+        makeAPIRequest("post", apiconst.createMotivation, formData, null, null)
+            .then((res) => {
+                console.log(res.data);
+                refClose.current.click()
+                getAllMotivations()
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
-    const [editMember, setEditMember] = useState({
-        // mukhiya_id: "",
-        // mukhiya_mobile_no: "",
-        // member_password: ""
-    })
+    // -------------------------- Add motivation ------------------------
+    
 
-    const ref = useRef(null);
+    //-----------------------remove motivation---------------------
 
-    const updateMember = (currentRest) => {
-        ref.current.click();
-        setEditMember({
-            mukhiya_id: currentRest.mukhiya_id,
-            mukhiya_mobile_no: currentRest.mukhiya_mobile_no,
-            member_password: currentRest.member_password
-        })
+    const deleteMotivation = (id) => {
+        makeAPIRequest("delete", apiconst.remove_Motivation + id, null, null)
+            .then((res) => {
+                getAllMotivations()
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
-    const handleSubmit = (e) => {
-        // updateAllMember(
-        //     editMember.mukhiya_id,
-        //     editMember.mukhiya_mobile_no,
-        //     editMember.member_password,
-        // )
-    }
+   // -------------------------------remove motivation----------------------
 
-    const editChange = (e) => {
-        setEditMember({ ...editMember, [e.target.name]: e.target.value })
-    }
 
+    useEffect(() => {
+        getAllMotivations();
+    }, [getAllMotivations])
 
     return (
         <>
@@ -77,84 +84,61 @@ const Motivation = () => {
                 <div className="total-rightsde-section">
                     <Navbar />
                     <div className="add-data">
-                        <button className='ad_slider_btn' type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <button className='ad_slider_btn' type="button" data-bs-toggle="modal" data-bs-target="#addMotivation">
                             + Add Data
                         </button>
                     </div>
                     <div className="data_table">
                         <table className="table table-striped oneeight">
-                            <thead style={{ borderBottom: '1px' }}>
+                            <thead>
                                 <tr>
-                                    <th scope="col" className='all-padding'>Index</th>
-                                    <th scope="col">Image</th>
-                                    <th scope="col">Notes</th>
-                                    <th scope="col">Motivation_by</th>
-
+                                    <th scope="col">Photo</th>
+                                    <th scope="col">Note</th>
+                                    <th scope="col">MOtivation By</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {/* {
-                                    allmembers?.map((item, index) => (
-                                        <tr key={index}>
-                                            <th scope="row" className='all-padding1'>{index + 1}</th>
-                                            <td>{item?.mukhiya_mobile_no}</td>
-                                            <td>{item?.member_id}</td>
-                                            <td>{item?.member_password}</td>
-                                            <td><Link to='/fetchdata'><AiFillEye className='cursor-pointer1' /></Link></td>
-                                            <td><BiEditAlt className='cursor-pointer' onClick={() => updateMember(item)} /></td>
-                                            <td><BsWhatsapp className='wp' /></td>
+                                {
+                                    motivation?.map((item, index) => (
+                                        <tr className='tbody-tr' key={index} data-toggle="modal" data-target="#exampleModal">
+                                            <th scope="row"><img src={apiconst.getAnyImages + item.photo} alt="photo" className='inner-photo' /></th>
+                                            <td>{item.notes}</td>
+                                            <td>{item.motivation_By}</td>
+                                            <td>{item.created_date.slice(0, 10)}</td>
+                                            <td><AiFillDelete onClick={() => deleteMotivation(item?.motivation_id)} /></td>
                                         </tr>
                                     ))
-                                } */}
+                                }
                             </tbody>
                         </table>
                     </div>
                 </div>
 
-                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal fade" id="addMotivation" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <div className="modal-header">
                                 <h4 className="modal-title" id="exampleModalLabel">Add New Data</h4>
-                                <button type="button" className="btn-close" ref={refClose1} data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                                <p className='modal-title-name'>Image</p>
-                                <input type="text" className='input-tag' onChange={onChangesholiday} name='mukhiya_mobile_no' />
-                                <p className='modal-title-name'>Notes</p>
-                                <input type="text" value={lastId} readOnly className='input-tag' name='member_id' />
-                                <p className='modal-title-name'>Motivation_by</p>
-                                <input type="password" className='input-tag' onChange={onChangesholiday} name='password' />
-                            </div>
-                            <div className="modal-footer">
-                                <button onClick={() => addData(lastId)} type="submit" className="ad_slider_btn2">Save changes</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <button type="button" style={{ display: 'none' }} ref={ref} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal123">
-                    Launch demo modal
-                </button>
-                <div className="modal fade" id="exampleModal123" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h4 className="modal-title" id="exampleModalLabel">Update Data</h4>
                                 <button type="button" className="btn-close" ref={refClose} data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
-                            <div className="modal-body">
-                                <p className='modal-title-name'>Mukhiya Mobile No</p>
-                                <input type="text" className='input-tag' onChange={editChange} name='mukhiya_mobile_no' value={editMember.mukhiya_mobile_no} />
-                                <p className='modal-title-name'>Password</p>
-                                <input type="text" className='input-tag' onChange={editChange} name='member_password' value={editMember.member_password} />
-                            </div>
-                            <div className="modal-footer">
-                                <button onClick={() => handleSubmit()} type="submit" className="ad_slider_btn2">Save changes</button>
-                            </div>
+                            <form onSubmit={addMotivationsByMotivators}>
+                                <div className="modal-body">
+                                    <p className='modal-title-name'>Image</p>
+                                    <input required type="file" className='input-tag' onChange={(e) => setMotivatorImg(e.target.files[0])} />
+                                    <p className='modal-title-name'>Notes</p>
+                                    <input required type="text" className='input-tag' name='notes' onChange={onChanges} />
+                                    <p className='modal-title-name'>Motivation_by</p>
+                                    <input required type="text" className='input-tag' name='motivation_By' onChange={onChanges} />
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="submit" onClick={addMotivationsByMotivators} className="ad_slider_btn2">Save changes</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-
             </div>
         </>
     )
